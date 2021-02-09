cbfb8eab1d29c3593a50a6c7828cd73a4e1a59a1
jeniuss <Jeniuss26@gmail.com>
2021-02-09 22:51:52 +0700

    Create sales order creation

---
+++ src/app/create-salesorder/create-salesorder.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'create-salesorder',
+  templateUrl: './create-salesorder.component.html',
+  styleUrls: ['./create-salesorder.component.css']
+})
+export class CreateSalesorderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
