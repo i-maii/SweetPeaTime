5177b88d957a72649f624d93f89aa88716e2ea9a
jeniuss <Jeniuss26@gmail.com>
2021-02-15 16:57:12 +0700

    Edit create salesorder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateSalesorderComponent } from './create-salesorder/create-salesorder.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { EditSalesorderComponent } from './edit-salesorder/edit-salesorder.component';
 import { SalesorderComponent } from './salesorder/salesorder.component';
 
 const routes: Routes = [
@@ -18,8 +19,8 @@ const routes: Routes = [
     component: CreateSalesorderComponent
   },
   {
-    path: 'createSalesorder',
-    component: CreateSalesorderComponent
+    path: 'editSalesorder',
+    component: EditSalesorderComponent
   }
 ];
 
