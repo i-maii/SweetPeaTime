3440de833eaf97c40485f0dbf59b0d32f1ab103b
Mac <pattrara.pj@gmail.com>
2021-03-08 08:57:04 +0700

    interface searchflowerFormulaResult

---
+++ src/app/interface/searchFlowerFormulaResult.ts
@@ -0,0 +1,14 @@
+export interface SearchFlowerFormulaResult {
+    id: number;
+    position: number; 
+    florist: string;
+    name: string;
+    size: string;
+    pattern: string;
+    color: string;
+    price: number;
+    occasion: string;
+    quantityAvailable: string;
+    deliveryFee: number;
+    totalPrice : number;
+}
